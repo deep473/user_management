1e606c50a6a5945b634c2d62017bfdf50054f051
deeptanshukumar <deeptanshukumar@localhost>
2026-04-09 12:36:57 +0530

    commit 2

--- user.management/src/main/java/com/project/user/management/service/UsersServiceImplementation.java
+++ user.management/src/main/java/com/project/user/management/service/UsersServiceImplementation.java
@@ -1,7 +1,12 @@
 package com.project.user.management.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.project.user.management.dto.UsersRequestDto;
+import com.project.user.management.dto.UsersResponseDto;
+import com.project.user.management.entity.Users;
 import com.project.user.management.repository.UsersRepository;
 
 @Service
@@ -13,6 +18,36 @@ public class UsersServiceImplementation
 	public UsersServiceImplementation(UsersRepository repo) {
 		super();
 		this.repo = repo;
+	}
+
+	@Override
+	public String register(UsersRequestDto user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public UsersResponseDto searchUser(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<UsersResponseDto> viewAllUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String updateUser(Users user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String deleteUser(Long id) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 }
